c358483402b8cd6edd99e9125046307ddd61d14c
Sandrava Philips <lacasandra1@gmail.com>
2020-02-28 11:45:55 +0100

    Stretch goal completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -40,15 +40,16 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None]* capacity
+        self.current = 0
 
     def append(self, item):
-        pass
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
+        
 
     def get(self):
-        pass
-
-buffer_2 = RingBuffer(5)
-for i in range(50):
-    buffer_2.append(i)
-print(buffer_2.get())
+        return [i for i in self.storage if i]
